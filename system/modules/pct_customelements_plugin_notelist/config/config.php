620a044da351fd585470c72102590f0e2b60831c
Tim Gatzky <info@tim-gatzky.de>
2016-01-25 08:55:30 +0100

    update to version 1.1.0

--- system/modules/pct_customelements_plugin_notelist/config/config.php
+++ system/modules/pct_customelements_plugin_notelist/config/config.php
@@ -16,7 +16,7 @@
  * Constants
  */ 
 define(PCT_CUSTOMELEMENTS_NOTELIST_PATH,'system/modules/pct_customelements_plugin_notelist');
-define(PCT_CUSTOMELEMENTS_NOTELIST_VERSION,'1.0.9');
+define(PCT_CUSTOMELEMENTS_NOTELIST_VERSION,'1.1.0');
 
 /**
  * Register plugin
